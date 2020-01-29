bd6718e0c578e337697f28d5c98e678322cc94fd
saenpsak <60366486+saenpsak@users.noreply.github.com>
2020-01-29 14:47:49 -0600

    Add files via upload
    Practice

---
+++ 1.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+import random
+plt.ion()
+
+def roll_hundred_pair():
+    total = []
+    for choice in range(100):
+        dice1 = random.randint(1,6)
+        dice2 = random.randint(1,6)
+        total += [(dice1+dice2)]
+    plt.hist(total)
+    plt.show()
